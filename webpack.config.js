f724e9dc917dcc1b3e9702dc06a01d1ebee5e2c4
Micah Allen <micahisnt@gmail.com>
2019-02-02 20:46:00 -0500

    Fix RAGE client-side export definition

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path  = require('path');
+const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
+
+const LIBRARY_NAME = 'rpc';
+const OUTPUT_FILE = 'rage-rpc.min.js';
 
 module.exports = {
     entry: './src/index.ts',
@@ -16,9 +20,19 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'rage-rpc.min.js',
-        library: 'rpc',
+        filename: OUTPUT_FILE,
+        library: LIBRARY_NAME,
         libraryTarget: 'umd',
         globalObject: "typeof self !== 'undefined' ? self : this"
-    }
+    },
+    plugins: [
+        new ReplaceInFileWebpackPlugin([{
+            dir: 'dist',
+            files: [OUTPUT_FILE],
+            rules: [{
+                search: `exports.${LIBRARY_NAME}`,
+                replace: 'exports'
+            }]
+        }])
+    ]
 };
